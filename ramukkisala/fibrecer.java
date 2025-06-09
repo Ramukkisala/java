3f7c50a67f4fcbb01e025ab6d7196432fa3d805f
Ramukkisala <ramukkisala94@gmail.com>
2025-06-09 15:32:26 +0530

    Add files via upload

---
+++ ramukkisala/fibrecer.java
@@ -0,0 +1,16 @@
+public class febrecur
+{
+    public int fun(int n)
+    {
+        if(n<=1)
+        {
+        return 1;
+        }
+        return fun(n-1)+fun(n-2);
+        
+        }
+	public static void main(String[] args) {
+		Main m=new Main();
+		System.out.println(m.fun(8));
+	}
+}
